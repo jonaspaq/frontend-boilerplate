e9779015e9cb38e1987f58954b26b96b0d87a2e2
Jonas <jonaspaq@gmail.com>
2020-01-29 15:12:40 +0800

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
+const browserSync = require('browser-sync').create();
+ 
+sass.compiler = require('node-sass');
+
+// Html
+gulp.task('html', function(){
+    return gulp.src('./src/*.html')
+    .pipe(gulp.dest('./dist/'))
+})
+
+// Scss compilation
+gulp.task('scss', function () {
+    return gulp.src('./src/assets/css/style.scss')
+    .pipe(sass({
+        outputStyle: 'compressed'
+    }).on('error', gulp.series(gulp.task('scss')), sass.logError))
+    .pipe(gulp.dest('./dist/assets/css/'))
+});
+
+// Watch Scss
+gulp.task('watch:scss', function(){
+    gulp.watch('src/assets/css/**/*.scss').on('change', gulp.series('scss', browserSync.reload))
+});
+
+// Image minify
+gulp.task('imagemin', function(){
+    return gulp.src('src/assets/images/*')
+        .pipe(imagemin()).on('error', gulp.series(gulp.task('imagemin')), imagemin.logError)
+        .pipe(gulp.dest('dist/assets/images'))
+})
+
+// Browser Sync
+gulp.task('serve', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./dist",
+            browsers: ['*']
+        }
+    });
+
+    // Watch these directories
+    gulp.watch("src/assets/images/**/*.*").on('change', gulp.series('imagemin', browserSync.reload));
+    gulp.watch("src/*.html").on('change', gulp.series('html', browserSync.reload));
+});
+
+
+gulp.task('default', gulp.parallel('html', 'scss', 'watch:scss', 'imagemin', 'serve'))
